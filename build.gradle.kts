59f40109b4fe02995663bbdbe8c34bdaad5e3b58
vladislavkhamin90 <vladislavkhamin90@gmail.com>
2025-09-17 19:26:30 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.9.22"
+    kotlin("jvm") version "1.9.0"
     application
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
@@ -14,17 +12,16 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:2.3.7")
-    implementation("io.ktor:ktor-server-netty:2.3.7")
-    implementation("io.ktor:ktor-server-content-negotiation:2.3.7")
-    implementation("io.ktor:ktor-serialization-gson:2.3.7")
-    implementation("io.ktor:ktor-server-websockets:2.3.7")
-    implementation("io.ktor:ktor-server-cors:2.3.7")
-    implementation("ch.qos.logback:logback-classic:1.4.14")
-    implementation("at.favre.lib:bcrypt:0.10.2")
+    implementation("io.ktor:ktor-server-core:2.3.0")
+    implementation("io.ktor:ktor-server-netty:2.3.0")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.0")
+    implementation("io.ktor:ktor-serialization-gson:2.3.0")
+    implementation("io.ktor:ktor-server-websockets:2.3.0")
+    implementation("io.ktor:ktor-server-cors:2.3.0")
+    implementation("ch.qos.logback:logback-classic:1.4.7")
 }
 
-tasks.withType<KotlinCompile> {
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
 }
 
