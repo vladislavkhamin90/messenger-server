295fa294ca6a7f307a56f98286aa59e5cde1d473
vladislavkhamin90 <vladislavkhamin90@gmail.com>
2025-12-17 18:04:09 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,37 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
     application
+    kotlin("jvm") version "1.9.0"
+    id("io.ktor.plugin") version "2.3.6"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
 }
 
-group = "com.yourname.messenger"
+group = "com.yourname"
 version = "1.0.0"
+
+application {
+    mainClass.set("com.yourname.messenger.ApplicationKt")
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:2.3.0")
-    implementation("io.ktor:ktor-server-netty:2.3.0")
-    implementation("io.ktor:ktor-server-content-negotiation:2.3.0")
-    implementation("io.ktor:ktor-serialization-gson:2.3.0")
-    implementation("io.ktor:ktor-server-websockets:2.3.0")
-    implementation("io.ktor:ktor-server-cors:2.3.0")
-    implementation("ch.qos.logback:logback-classic:1.4.7")
+    implementation("io.ktor:ktor-server-core:2.3.6")
+    implementation("io.ktor:ktor-server-netty:2.3.6")
+    implementation("io.ktor:ktor-server-cors:2.3.6")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.6")
+    implementation("io.ktor:ktor-serialization-gson:2.3.6")
+    implementation("io.ktor:ktor-server-websockets:2.3.6")
+    
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+    
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    
+    testImplementation("io.ktor:ktor-server-test-host:2.3.6")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.9.0")
 }
 
-application {
-    mainClass.set("com.yourname.messenger.ApplicationKt")
-}
-
-tasks {
-    jar {
-        manifest {
-            attributes("Main-Class" to "com.yourname.messenger.ApplicationKt")
-        }
-        
-        from(configurations.runtimeClasspath.get().map { 
-            if (it.isDirectory) it else zipTree(it) 
-        })
-        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    }
+kotlin {
+    jvmToolchain(17)
 }
