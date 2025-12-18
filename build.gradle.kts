06dcd52bda9c5cb42cebb93537fdfafb28358173
vladislavkhamin90 <vladislavkhamin90@gmail.com>
2025-12-18 16:20:26 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,24 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.serialization") version "1.9.22"
-    application
+    kotlin("jvm") version "1.9.22" 
+    application  
+}
+
+repositories {
+    mavenCentral() 
 }
 
 dependencies {
     implementation("io.ktor:ktor-server-core:2.3.8")
     implementation("io.ktor:ktor-server-netty:2.3.8")
+    
     implementation("io.ktor:ktor-server-content-negotiation:2.3.8")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.8")
+    
     implementation("io.ktor:ktor-server-cors:2.3.8")
-    implementation("io.ktor:ktor-server-auth:2.3.8")
-    implementation("io.ktor:ktor-server-auth-jwt:2.3.8")
-    
-    // База данных
-    implementation("org.postgresql:postgresql:42.7.2")
-    implementation("org.jetbrains.exposed:exposed-core:0.45.0")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.45.0")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.45.0")
-    
-    // Пароль
-    implementation("at.favre.lib:bcrypt:0.10.2")
     
     implementation("ch.qos.logback:logback-classic:1.4.14")
+    
+    implementation("at.favre.lib:bcrypt:0.10.2")
 }
 
 application {
