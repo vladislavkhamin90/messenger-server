faeb38e95f3f2cd27b4a13b4c779d9fac54419c4
vladislavkhamin90 <vladislavkhamin90@gmail.com>
2025-12-18 16:54:34 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.9.22" 
-    application  
+    kotlin("jvm") version "1.9.22"
+    application
 }
 
 repositories {
-    mavenCentral() 
+    mavenCentral()
 }
 
 dependencies {
@@ -30,3 +30,19 @@ application {
     mainClass.set("ApplicationKt")
 }
 
+tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
+        archiveClassifier.set("standalone")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to application.mainClass.get())) }
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+                sourcesMain.output
+        from(contents)
+    }
+    build {
+        dependsOn(fatJar)
+    }
+}
